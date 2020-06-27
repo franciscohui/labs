f9006883c9b6f514957e10a0e3150d20f34de84a
franciscohui <franciscohui@users.noreply.github.com>
2020-06-27 16:31:21 -0400

    Add weather app
    Shows hourly weather for a fixed location.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Projects from './Projects.js'
+import Weather from './Weather.js'
 import './App.css';
 
 function App() {
   return (
     <div>
       <h1>2020 Projects</h1>
+      <Weather/>
       <Projects />
     </div>
   )
---
+++ src/Weather.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import "./Weather.css"
+
+/*
+
+nyc lat:  40.7128
+nyc long: -74.006
+
+"gridId": "OKX",
+     "gridX": 32,
+     "gridY": 34,
+*/
+
+function Weather() {
+  const [weather, setWeather] = useState();
+
+  function fetchWeather() {
+    const weatherHourlyForecast =
+      "https://api.weather.gov/gridpoints/OKX/32,34/forecast/hourly";
+
+    fetch(weatherHourlyForecast)
+      .then(initialResponse => initialResponse.json())
+      .then(responseJSON => {
+        console.log(responseJSON.properties.periods);
+        setWeather(HourlyWeather(responseJSON.properties.periods));
+      });
+  }
+
+  function HourlyWeather(props) {
+
+    /* Formating Timestamp
+    const timeStamp = new Date(props[0].startTime).getDate();
+    console.log(timeStamp);
+    console.log("Date:" + timeStamp.getDate() + " Hour: " + timeStamp.getHours())
+    */
+
+    const eachHour = props.map((hour) => (
+      <WeatherRow
+        id={hour.number}
+        temperature={hour.temperature}
+        time={new Date(hour.startTime).getHours()}
+        daytime={hour.isDaytime}
+      />
+    ));
+    return <ul className="hourlyWeather">{eachHour}</ul>;
+  }
+
+  function WeatherRow({id, time, temperature, daytime}){
+    const height = {height: temperature-30 + 'vh'}
+    const classVariables = (daytime? 'day' : 'night') + ' hourlyRow';
+
+    return(
+        <li className={classVariables} key={id} style={height}>
+          <div>{temperature}°</div>
+          <div className="time">{time}</div>
+        </li>
+    )
+  }
+
+  return (
+    <div>
+      <button onClick={fetchWeather}>Show weather</button>
+      <div className="weather">{weather}</div>
+    </div>
+  );
+}
+
+export default Weather;
